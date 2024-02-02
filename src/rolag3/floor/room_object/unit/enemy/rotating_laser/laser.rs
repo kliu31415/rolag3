39e2b0c3591a4ef6c7033c4ccdcdc44e4478e9cf
Kevin Liu <kliu31415@gmail.com>
2024-02-01 21:02:31 -0800

    chunk rotating laser as a Rofiz optimization

--- src/rolag3/floor/room_object/unit/enemy/rotating_laser/laser.rs
+++ src/rolag3/floor/room_object/unit/enemy/rotating_laser/laser.rs
@@ -15,7 +15,7 @@ struct RotatingLaserBasic {
     xformed_rect_cache: Shape,
     angular_speed: f64,
 
-    rotating_laser_rofo_ref: RofizObjectRef,
+    rotating_laser_rofo_refs: Vec<RofizObjectRef>,
 }
 
 pub fn new_rotating_laser(
@@ -37,7 +37,6 @@ pub fn new_rotating_laser(
         DamageColor::Blue => Color::new(0.01, 0.01, 0.5, 1.0),
         _ => panic!("can't create rotating laser with color {:?}", color),
     };
-    let rect = Shape::of_rect(Rect::new(0.0, -LASER_WIDTH / 2.0, laser_length as f32, LASER_WIDTH));
 
     let mut builder = StandardUnit1Builder::new(StandardUnit1BuilderReq {
         team: Team::Enemy,
@@ -47,16 +46,27 @@ pub fn new_rotating_laser(
         tire_traction: 1.0, // dummy
     });
     let md = builder.get_room_obj_metadata(ctx);
-    // TODO: if laser_length is large, break up the laser into multiple Rofiz objects as an optimizations
-    let rotating_laser_rofo_ref = ctx.add_basic_projectile(md.get_ref(), Hitbox::new(xform, rect.clone()));
+
+    // the laser is split up into multiple chunks of length <=5 as a Rofiz optimization. Recall that Rofiz checks a
+    // projectile against all grid cells in its bounding box. The bounding boxes of chunks of a big, thin rectangle
+    // are smaller in total area than the bounding box of the big, thin rectangle if the rectangle isn't axis-aligned.
+    assert!(laser_length > 0.0, "expected laser_length({}) > 0", laser_length);
+    let num_chunks = 1 + (laser_length / 5.0) as usize;
+    let mut rotating_laser_rofo_refs = Vec::new();
+    for i in 0..num_chunks {
+        let x = (i as f64) * laser_length / (num_chunks as f64);
+        let w = laser_length / (num_chunks as f64);
+        let rect = Shape::of_rect(Rect::new(x as f32, -LASER_WIDTH / 2.0, w as f32, LASER_WIDTH));
+        rotating_laser_rofo_refs.push(ctx.add_basic_projectile(md.get_ref(), Hitbox::new(xform, rect.clone())));
+    }
 
     let us_data = RotatingLaserBasic {
         laser_draw_color: draw_color,
         circular_base_inner_draw_color,
-        rect: rect.clone(),
+        rect: Shape::of_rect(Rect::new(0.0, -LASER_WIDTH / 2.0, laser_length as f32, LASER_WIDTH)),
         xformed_rect_cache: Shape::default(),
         angular_speed,
-        rotating_laser_rofo_ref,
+        rotating_laser_rofo_refs,
     };
 
     builder.act1_fn(Box::new(act1))
@@ -71,13 +81,15 @@ pub fn new_rotating_laser(
 fn act1(ctx: &mut SuAct1Context) -> Act1Response {
     let us_data = ctx.su_ctx.us_data.downcast_mut::<RotatingLaserBasic>().unwrap();
     let move_xform = Transformation::new(0.0, 0.0, ctx.su_ctx.su_common.get_unit_tick_len() * us_data.angular_speed);
-    ctx.act1_ctx.get_rofiz().move_object(&us_data.rotating_laser_rofo_ref, RofizObjectMovement::Move(move_xform));
+    for rr in us_data.rotating_laser_rofo_refs.iter() {
+        ctx.act1_ctx.get_rofiz().move_object(rr, RofizObjectMovement::Move(move_xform));
+    }
     Act1Response::new()
 }
 
 fn draw(ctx: &mut SuDrawContext) {
     let us_data = ctx.su_ctx.us_data.downcast_mut::<RotatingLaserBasic>().unwrap();
-    let xform = ctx.draw_ctx.get_rofiz().get_movable_object_xform(&us_data.rotating_laser_rofo_ref);
+    let xform = ctx.draw_ctx.get_rofiz().get_movable_object_xform(&us_data.rotating_laser_rofo_refs[0]);
     xform.replace_shape_with_transformed(&mut us_data.xformed_rect_cache, &us_data.rect);
     let Shape::Polygon(xformed_rect) = &us_data.xformed_rect_cache else {panic!()};
     let circular_base = ctx.draw_ctx.do_concentric_circle(
